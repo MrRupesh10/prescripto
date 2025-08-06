f66f3af4e1a9e0210a0afc26bb98c9d3b57f5c90
RupeshYadav <balajirupesh10@gmail.com>
2025-08-07 01:47:25 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>robot@gmail.com</li>
+            <li>+91-8528399380</li>
+            <li>rupeshyadav1b@gmail.com</li>
           </ul>
         </div>
 
